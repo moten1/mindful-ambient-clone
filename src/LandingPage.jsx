87834897405ef8c2d09ba960d357ba7dc79ab157
Kashif Moten <energyclinicglobal@gmail.com>
2025-06-24 15:12:11 +0500

    Fixed main.jsx route and import

---
+++ src/LandingPage.jsx
@@ -0,0 +1,7 @@
+import LandingPage from "./LandingPage.jsx";
+import MainApp from "./mainapp.jsx";
+
+<Routes>
+  <Route path="/" element={<LandingPage />} />
+  <Route path="/app" element={<MainApp />} />
+</Routes>
--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,17 @@
 
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import LandingPage from "./LandingPage.jsx"; // 🏠 Luxury Homepage
+import MainApp from "./mainapp.jsx";         // 🤖 AI-Powered Assistant
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/app" element={<MainApp />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
